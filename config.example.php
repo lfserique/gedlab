2612059228116fd78d45ce37f94c164c31aad409
lfserique <serique@gmail.com>
2026-04-18 10:29:08 -0300

    Update database password in config example

--- config.example.php
+++ config.example.php
@@ -2,7 +2,7 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'gedlab');
 define('DB_USER', 'geduser');
-define('DB_PASS', 'SUA_SENHA_AQUI');
+define('DB_PASS', 'Z9#vL2!qR7@pX4$mT8&c');
 
 define('APP_NAME', 'GEDLab');
 define('APP_URL', 'http://127.0.0.1:8000');
